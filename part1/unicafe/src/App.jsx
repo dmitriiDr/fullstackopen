c92102426e1d412ebd90e80d49dcfd499a21e8e8
Dmitrii Druzhbin <dmitrii.druzhbin@halias.fr>
2026-02-01 13:03:51 +0100

    task 1.6 fin

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,9 +6,22 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const Button = ({onClick, text}) => (
+    <button onClick={onClick}>{text}</button>
+  )
+
+
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <h1>statistics</h1>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
     </div>
   )
 }
